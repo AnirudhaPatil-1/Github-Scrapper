24094182d7c6d5f689f57dde02263e802df87436
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-01-03 16:25:58 +0530

    started project: github scrapper

---
+++ steps.js
@@ -0,0 +1,8 @@
+//npm init -y
+//npm install request cheerio
+//create file ".gitignore" and put node_modules in it
+//initialize the git repository and push it to github
+// git commands ->
+// git init
+//git status, git add, git commit -m "commit message"
+// git push
